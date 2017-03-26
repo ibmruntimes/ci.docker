9b65a925534ce0c6119c6a21cc3967e6250148ca
Dinakar Guniguntala <dinogun@gmail.com>
2017-03-26 18:16:59 +0530

    Add additional tags for 9 beta images. Commit the correct update_repo.sh file this time!

--- ibmjava/tools/update_repo.sh
+++ ibmjava/tools/update_repo.sh
@@ -18,9 +18,9 @@ set -o pipefail
 
 function usage() {
 	echo
-	echo "Usage: $0 [-h] [-i <j|t|b>] [-l] [-n] [-v <8|9>] [-s source_repo] [-t <ibmcom|ppc64le|s390x>]"
+	echo "Usage: $0 [-h] [-i <java|tools|both>] [-l] [-n] [-v <8|9>] [-s source_repo] [-t <ibmcom|ppc64le|s390x>]"
 	echo " h = help. "
-	echo " i = Images to be pushed, j = java only, t = tools only, default = both. "
+	echo " i = Images to be pushed: 'java' only, 'tools' only, default = 'both'. "
 	echo " l = use local source. "
 	echo " n = no push to remote. "
 	echo " s = source repo, default = j9. "
@@ -41,9 +41,9 @@ machine=`uname -m`
 push_repo="ibmjava"
 nopush=0
 
-unset jvar
+unset jver
 tools="maven"
-image="b"
+image="both"
 
 # Setup defaults for source and target repos based on the current machine arch.
 case $machine in
@@ -76,7 +76,7 @@ do
 	i)
 		# which images to push.
 		image="$OPTARG"
-		[[ $image == "j" || $image == "t"  || $image == "b" ]] || usage
+		[[ $image == "java" || $image == "tools"  || $image == "both" ]] || usage
 		;;
 	l)
 		# Use local repo instead of pulling from remote source.
@@ -141,7 +141,7 @@ function retag() {
 	fi
 	# Create the target image locally with the appropriate tag from the source.
 	log -n "####[I]: [$source_repo:$stag]: Tagging image as [$target_repo:$ttag]..."
-	#docker tag $source_repo:$stag $target_repo:$ttag >> $logfile 2>&1
+	docker tag $source_repo:$stag $target_repo:$ttag >> $logfile 2>&1
 	log "done"
 }
 
@@ -152,7 +152,7 @@ function push_target() {
 		log
 		# Push the target images to the remote
 		log "####[I]: [$target_repo:$ttag]: Pushing image to hub.docker..."
-		#docker push $target_repo:$ttag >> $logfile 2>&1
+		docker push $target_repo:$ttag >> $logfile 2>&1
 		checkFail "####[E]: [$target_repo:$ttag]: Image push failed, check the target repository provided.\n" \
 				  "####[E]: Are you logged-in,  does your userid have push authorization?"
 		log "####[I]: [$target_repo:$ttag]: Image pushed successfully."
@@ -167,7 +167,7 @@ function get_source_image() {
 	# If source is remote, pull images from remote, else check if image is available locally.
 	if [ $remote -eq 1 ]; then
 		log -n "####[I]: [$repo:$stag]: Pulling image..."
-		#docker pull $repo:$stag >> $logfile 2>&1
+		docker pull $repo:$stag >> $logfile 2>&1
 		checkFail "\n####[E]: [$repo:$stag]: Pulling image failed, exiting..."
 		log "done"
 	else
@@ -186,7 +186,7 @@ function update_target() {
 	ttag=$2
 	
 	# Ignore alpine on non x86_64
-	if [[ "$stag" ==*alpine ]]; then
+	if [[ "$stag" == *alpine ]]; then
 		if [ $machine != "x86_64" ]; then
 			return;
 		fi
@@ -244,9 +244,9 @@ function read_file() {
 # Read from the tags file and create and array of tags to be created and pushed.
 # java_tags.txt consists of all tags for various java versions.
 # tool_tags.txt consists of all tags for various tools and related versions.
-if [ $image == 'j' ]; then
+if [ $image == "java" ]; then
 	read_file java_tags.txt
-elif [ $image == 't' ]; then
+elif [ $image == "tools" ]; then
 	read_file tool_tags.txt
 else
 	read_file java_tags.txt
