03abab4bb568a02129fedf0cfe79584be477bbcb
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2017-10-17 13:45:12 +0100

    Move Dockerfiles into their own dirs

--- ibmnode/update.sh
+++ ibmnode/update.sh
@@ -10,4 +10,4 @@ sed -e "s/{{IBM_VERSION}}/$IBM_VERSION/g" \
 -e "s/{{NODE_VERSION}}/$NODE_VERSION/g" \
 -e "s/{{AMD64_SHA}}/$AMD64_SHA/g" \
 -e "s/{{PPC64LE_SHA}}/$PPC64LE_SHA/g" \
--e "s/{{S390X_SHA}}/$S390X_SHA/g" Dockerfile-base  > Dockerfile-$MAJOR_VERSION
+-e "s/{{S390X_SHA}}/$S390X_SHA/g" Dockerfile-base  > $MAJOR_VERSION/Dockerfile
