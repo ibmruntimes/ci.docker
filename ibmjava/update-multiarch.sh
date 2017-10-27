e1eb066c7d9360c53574f75f367d0a2c53ffe1d9
Dinakar Guniguntala <dinakar.g@in.ibm.com>
2017-10-27 23:42:39 +0530

    Remove extraneous dir in all 6 images. (#16)

--- ibmjava/update-multiarch.sh
+++ ibmjava/update-multiarch.sh
@@ -224,7 +224,9 @@ EOI
 
 	# For other Java versions, nothing to be done.
 	else
-		JCMD="rm -f /tmp/ibm-java.bin;"
+		JCMD="rm -f /tmp/ibm-java.bin; \\
+    cd /opt/ibm/java/jre/lib; \\
+    rm -rf icc;"
 	fi
 
 	cat >> $1 <<EOI
